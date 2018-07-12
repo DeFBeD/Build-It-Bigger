24d3a7161491a09db07ba27c738f83d7529ea2e7
Jose Burgos <jburgos7705@gmail.com>
2018-07-12 10:54:31 -0400

    added joke randomizer and new manifest for flavors

--- jokeProvider/src/main/java/com/example/android/jokeprovider/GoodJokes.java
+++ jokeProvider/src/main/java/com/example/android/jokeprovider/GoodJokes.java
@@ -1,8 +1,24 @@
 package com.example.android.jokeprovider;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class GoodJokes {
 
     public String tellAGoodJoke(){
-        return "Why Should you stand in the corner if it gets cold?...Because its always 90 degrees!";
+        Random randomGenerator = new Random();
+        List<String> jokeList = Arrays.asList(
+                "Why Should you stand in the corner if it gets cold?  \nBecause its always 90 degrees!",
+                "How do you keep a man from drowning?  \nget his wife off his back!",
+                "What does a computer eat for a snack? \nMicrochips!",
+                "What computer sings the best?\nA Dell!",
+                "What's Forrest Gumps's password ?\n1forrest1",
+                "What did the spider do on a computer?\nMade a website!"
+        );
+        int next = randomGenerator.nextInt(jokeList.size());
+        return jokeList.get(next);
     }
+
+
 }
